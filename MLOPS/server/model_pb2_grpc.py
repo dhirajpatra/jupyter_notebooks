0444eb931bfcb8010322a15ce326998a3f313ba5
Dhiraj Patra <dhiraj.patra@gmail.com>
2026-04-13 06:47:58 +0530

    updated

--- MLOPS/server/model_pb2_grpc.py
+++ MLOPS/server/model_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 import model_pb2 as model__pb2
 
-GRPC_GENERATED_VERSION = '1.76.0'
+GRPC_GENERATED_VERSION = '1.80.0'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
